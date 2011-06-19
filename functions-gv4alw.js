a8345aeaf03bc8384decca1efa3946a70c00d1db
Andrwe Lord Weber <github@andrwe.org>
2011-06-19 11:27:04 +0200

    added js for selecting sticky_notes-button
    Signed-off-by: Andrwe Lord Weber <github@andrwe.org>

--- functions-gv4alw.js
+++ functions-gv4alw.js
@@ -5,6 +5,8 @@ function rcube_init_plugin_tab()
     tab = '#settingstab' + rcmail.env.action.replace(/\./g, '');
   if (window.rcmail && (rcmail.env.task == 'help'))
     tab = 'a.button-' + rcmail.env.task;
+  if (window.rcmail && (rcmail.env.action == 'plugin.sticky_notes'))
+    tab = 'a.button-' + rcmail.env.action.replace(/.*\./g, '');
 
   $(tab).addClass('tablink-selected');
   $(tab + '> a').removeAttr('onclick').unbind('click').bind('click', function(){return false;});
