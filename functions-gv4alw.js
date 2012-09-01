ed27a0e7ceec8febf4e6a5539161701832bbfc5a
Andrwe Lord Weber <github@andrwe.org>
2012-09-01 02:03:57 +0200

    Version 0.8.1
    update tabbar
    redesign settingstab
    changed selected-class from span to link
    
    Signed-off-by: Andrwe Lord Weber <github@andrwe.org>

--- functions-gv4alw.js
+++ functions-gv4alw.js
@@ -2,7 +2,7 @@
 function rcube_init_plugin_tab()
 {
   if (window.rcmail && rcmail.env.action)
-    tab = '#settingstab' + rcmail.env.action.replace(/\./g, '');
+    tab = '#settingstab' + rcmail.env.action.replace(/\./g, '') + ' > a';
   if (window.rcmail && (rcmail.env.task == 'help'))
     tab = 'a.button-' + rcmail.env.task;
   if (window.rcmail && (rcmail.env.action == 'plugin.sticky_notes' || rcmail.env.action == 'plugin.calendar'))
