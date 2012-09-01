415867c722e455033bcb15fade34e90ba02d371b
Andrwe Lord Weber <github@andrwe.org>
2012-09-01 03:45:09 +0200

    Version 0.8.1
    finished tabsbar
    implemented js-function rcube_init_action_tab() for control of selected tabs in non-tabsbar pages
    activated rcube_init_action_tab() on identity edit
    
    Signed-off-by: Andrwe Lord Weber <github@andrwe.org>

--- functions-gv4alw.js
+++ functions-gv4alw.js
@@ -1,14 +1,21 @@
 
 function rcube_init_plugin_tab()
 {
-  if (window.rcmail && rcmail.env.action)
-    tab = '#settingstab' + rcmail.env.action.replace(/\./g, '') + ' > a';
-  if (window.rcmail && (rcmail.env.task == 'help'))
+  if (window.rcmail && rcmail.env.task)
     tab = 'a.button-' + rcmail.env.task;
-  if (window.rcmail && (rcmail.env.action == 'plugin.sticky_notes' || rcmail.env.action == 'plugin.calendar'))
+  if (window.rcmail && rcmail.env.task == 'settings' && rcmail.env.action)
+    tab = '#settingstab' + rcmail.env.action.replace(/\./g, '') + ' > a';
+    if (rcmail.env.action == 'folders' || rcmail.env.action == 'identities' || rcmail.env.action == 'add-identity' || rcmail.env.action == 'edit-identity')
+      tab = 'a.button-' + rcmail.env.action;
+  if (window.rcmail && rcmail.env.task == 'dummy' && (rcmail.env.action == 'plugin.sticky_notes' || rcmail.env.action == 'plugin.calendar'))
     tab = 'a.button-' + rcmail.env.action.replace(/.*\./g, '');
 
   $(tab).addClass('tablink-selected');
-  $(tab + '> a').removeAttr('onclick').unbind('click').bind('click', function(){return false;});
+  $(tab).removeAttr('onclick').unbind('click').bind('click', function(){return false;}).attr('onclick', '');
 }
 
+function rcube_init_action_tab()
+{
+  $('span.tablink > a').removeClass('tablink-selected').attr('onmousemove', 'rcube_init_action_tab()');
+  $('span.tablink-selected > a').addClass('tablink-selected');
+}
